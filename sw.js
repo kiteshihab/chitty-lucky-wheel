acd3907d38ca1fe019119320eed2941519016881
kiteshihab <kiteshihab@gmail.com>
2026-08-14 14:49:04 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const C="chitty-wheel-v1",A=["./","./index.html","./manifest.webmanifest","./icon.svg"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
